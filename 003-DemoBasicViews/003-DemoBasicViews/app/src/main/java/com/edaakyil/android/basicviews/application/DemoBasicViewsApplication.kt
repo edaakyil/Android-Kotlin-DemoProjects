a966121c7ff09f3005eccd345bfc954a9376a940
edaakyil <edaakyil@gmail.com>
2025-02-02 12:22:52 +0300

    Created a directory in the internal memory of application

--- 003-DemoBasicViews/003-DemoBasicViews/app/src/main/java/com/edaakyil/android/basicviews/application/DemoBasicViewsApplication.kt
+++ 003-DemoBasicViews/003-DemoBasicViews/app/src/main/java/com/edaakyil/android/basicviews/application/DemoBasicViewsApplication.kt
@@ -2,10 +2,12 @@ package com.edaakyil.android.basicviews.application
 
 import android.app.Application
 import android.widget.Toast
+import java.io.File
 
 class DemoBasicViewsApplication : Application() {
     override fun onCreate() {
         Toast.makeText(this, "Application started", Toast.LENGTH_LONG).show()
+        File(filesDir, "users").mkdirs()
         super.onCreate()
     }
 }
