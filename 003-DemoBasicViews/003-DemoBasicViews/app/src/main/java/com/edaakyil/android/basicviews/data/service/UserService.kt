de65f964bc06846d5b0aa897f0e6ee8364c723f8
edaakyil <edaakyil@gmail.com>
2025-02-05 16:28:37 +0300

    Filter users by specific condition/s

--- 003-DemoBasicViews/003-DemoBasicViews/app/src/main/java/com/edaakyil/android/basicviews/data/service/UserService.kt
+++ 003-DemoBasicViews/003-DemoBasicViews/app/src/main/java/com/edaakyil/android/basicviews/data/service/UserService.kt
@@ -25,53 +25,6 @@ private const val DELIMITER = ":"
 class UserService(context: Context) {
     private val mContext = context
 
-    private fun writeUserRegisterInfo(bw: BufferedWriter, userRegisterInfo: UserRegisterInfoModel) {
-        bw.write("${userRegisterInfo.username}$DELIMITER")
-        bw.write("${userRegisterInfo.name}$DELIMITER")
-        bw.write("${userRegisterInfo.email}$DELIMITER")
-        bw.write("${userRegisterInfo.maritalStatus}$DELIMITER")
-        bw.write("${userRegisterInfo.lastEducationDegree}")
-    }
-
-    fun saveUserData(userRegisterInfo: UserRegisterInfoModel) {
-        try {
-            val username = userRegisterInfo.username
-            BufferedWriter(OutputStreamWriter(FileOutputStream(File(mContext.filesDir, USERS_FORMAT.format("$username.txt"))), StandardCharsets.UTF_8))
-                .use { writeUserRegisterInfo(it, userRegisterInfo) }
-        } catch (ex: IOException) {
-            throw DataServiceException("UserService.saveUserData", ex)
-        }
-    }
-
-    private fun readRegisteringUserInfo(br: BufferedReader, username: String): UserRegisterInfoModel {
-        val str = br.readLine() ?: throw IOException()  // line(satır) okuma
-        val info = str.split(DELIMITER)  // line'ı split etme
-
-        return UserRegisterInfoModel(info[1], username, info[2], info[3][0], info[4].toInt())
-    }
-
-    fun findByUsername(username: String): UserRegisterInfoModel? {
-        try {
-            val file = File(mContext.filesDir, USERS_FORMAT.format("$username.txt"))
-
-            return if (file.exists()) BufferedReader(InputStreamReader(FileInputStream(file), StandardCharsets.UTF_8)).use { readRegisteringUserInfo(it, username) } else null
-        } catch (ex: IOException) {
-            throw DataServiceException("UserService.findByUsername", ex)
-        }
-    }
-
-    fun registerUserInfo(userRegisterInfo: UserRegisterInfoModel) {
-        try {
-            // Her registerUserInfo çağrıldığında data'lar farklı ObjectOutputStream'ler kullanılarak yazılıyor.
-            // Böylelikle her bir data farklı ObjectOutputStream'ler kullanılarak yaratılıyor.
-            // Yani her registerUserInfo çağrısında yeni bir ObjectOutputStream yaratılıyor
-            ObjectOutputStream(FileOutputStream(File(mContext.filesDir, USERS_FILE_PATH), true)).use { it.writeObject(userRegisterInfo) }
-            //File(mContext.filesDir, USERS_FORMAT.format("${userRegisterInfo.username}.txt")).delete()  // oguzkaran
-        } catch (ex: IOException) {
-            throw DataServiceException("UserService.registerUser", ex)
-        }
-    }
-
     /**
      * For Register user
      *
@@ -86,6 +39,7 @@ class UserService(context: Context) {
         }
     }
 
+    // Callback alan bir fonksiyon
     private fun userFilterCallback(fis: FileInputStream, predicate: (UserRegisterInfoModel) -> Boolean): Boolean {
         var result = false
 
@@ -104,6 +58,24 @@ class UserService(context: Context) {
         }
 
         return result
+    }
+
+    private fun findUsersByFilter(fis: FileInputStream, predicate: (UserRegisterInfoModel) -> Boolean): List<UserRegisterInfoModel> {
+        val users = ArrayList<UserRegisterInfoModel>()
+
+        try {
+            while (true) {
+                val ois = ObjectInputStream(fis)
+                val ri = ois.readObject() as UserRegisterInfoModel
+
+                if (predicate(ri))
+                    users.add(ri)
+            }
+        } catch (_: EOFException) {
+
+        }
+
+        return users
     }
 
     /*
@@ -151,6 +123,53 @@ class UserService(context: Context) {
             return FileInputStream(File(mContext.filesDir, USERS_FILE_PATH)).use { userFilterCallback(it) { it.username == username && it.password == password } }
         } catch (ex: IOException) {
             throw DataServiceException("UserService.existsByUsernameAndPassword", ex)
+        }
+    }
+
+    private fun readRegisteringUserInfo(br: BufferedReader, username: String): UserRegisterInfoModel {
+        val str = br.readLine() ?: throw IOException()  // line(satır) okuma
+        val info = str.split(DELIMITER)  // line'ı split etme
+
+        return UserRegisterInfoModel(info[1], username, info[2], info[3][0], info[4].toInt())
+    }
+
+    fun findByUsername(username: String): UserRegisterInfoModel? {
+        try {
+            val file = File(mContext.filesDir, USERS_FORMAT.format("$username.txt"))
+
+            return if (file.exists()) BufferedReader(InputStreamReader(FileInputStream(file), StandardCharsets.UTF_8)).use { readRegisteringUserInfo(it, username) } else null
+        } catch (ex: IOException) {
+            throw DataServiceException("UserService.findByUsername", ex)
+        }
+    }
+
+    private fun writeUserRegisterInfo(bw: BufferedWriter, userRegisterInfo: UserRegisterInfoModel) {
+        bw.write("${userRegisterInfo.username}$DELIMITER")
+        bw.write("${userRegisterInfo.name}$DELIMITER")
+        bw.write("${userRegisterInfo.email}$DELIMITER")
+        bw.write("${userRegisterInfo.maritalStatus}$DELIMITER")
+        bw.write("${userRegisterInfo.lastEducationDegree}")
+    }
+
+    fun saveUserData(userRegisterInfo: UserRegisterInfoModel) {
+        try {
+            val username = userRegisterInfo.username
+            BufferedWriter(OutputStreamWriter(FileOutputStream(File(mContext.filesDir, USERS_FORMAT.format("$username.txt"))), StandardCharsets.UTF_8))
+                .use { writeUserRegisterInfo(it, userRegisterInfo) }
+        } catch (ex: IOException) {
+            throw DataServiceException("UserService.saveUserData", ex)
+        }
+    }
+
+    fun registerUserInfo(userRegisterInfo: UserRegisterInfoModel) {
+        try {
+            // Her registerUserInfo çağrıldığında data'lar farklı ObjectOutputStream'ler kullanılarak yazılıyor.
+            // Böylelikle her bir data farklı ObjectOutputStream'ler kullanılarak yaratılıyor.
+            // Yani her registerUserInfo çağrısında yeni bir ObjectOutputStream yaratılıyor
+            ObjectOutputStream(FileOutputStream(File(mContext.filesDir, USERS_FILE_PATH), true)).use { it.writeObject(userRegisterInfo) }
+            //File(mContext.filesDir, USERS_FORMAT.format("${userRegisterInfo.username}.txt")).delete()  // oguzkaran
+        } catch (ex: IOException) {
+            throw DataServiceException("UserService.registerUser", ex)
         }
     }
 }
