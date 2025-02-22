7beca76fba30a600d25a092a5ed5a151ecc8c1df
edaakyil <edaakyil@gmail.com>
2025-02-22 16:01:26 +0300

    Bind the main layout of MainActivity

--- 004-DemoDataBindingApp/app/src/main/java/com/edaakyil/android/demodatabindingapp/MainActivity.kt
+++ 004-DemoDataBindingApp/app/src/main/java/com/edaakyil/android/demodatabindingapp/MainActivity.kt
@@ -43,7 +43,7 @@ class MainActivity : AppCompatActivity() {
         mBinding.loginButtonEnable = false
         mBinding.nonAnonymousAreasEnable = true
         mBinding.mainActivityLinearLayoutLoginArea.visibility = View.GONE
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainActivityLinearLayoutMain)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(mBinding.mainActivityLinearLayoutMain) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
