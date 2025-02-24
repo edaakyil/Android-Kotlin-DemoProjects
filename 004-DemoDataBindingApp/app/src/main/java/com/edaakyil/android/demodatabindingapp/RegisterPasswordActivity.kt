ccbf0bd1cba690373e812fd4b8b33495bbface71
edaakyil <edaakyil@gmail.com>
2025-02-24 13:46:11 +0300

    Bind all elements and events of RegisterPasswordActivity

--- 004-DemoDataBindingApp/app/src/main/java/com/edaakyil/android/demodatabindingapp/RegisterPasswordActivity.kt
+++ 004-DemoDataBindingApp/app/src/main/java/com/edaakyil/android/demodatabindingapp/RegisterPasswordActivity.kt
@@ -1,9 +1,6 @@
 package com.edaakyil.android.demodatabindingapp
 
 import android.os.Bundle
-import android.view.View
-import android.widget.EditText
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -12,9 +9,11 @@ import androidx.core.view.WindowInsetsCompat
 import android.os.Build
 import android.util.Log
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import com.edaakyil.android.demodatabindingapp.constant.USER_INFO
 import com.edaakyil.android.demodatabindingapp.constant.USERS_FORMAT
 import com.edaakyil.android.demodatabindingapp.data.service.UserService
+import com.edaakyil.android.demodatabindingapp.databinding.ActivityRegisterPasswordBinding
 import com.edaakyil.android.demodatabindingapp.model.UserRegisterInfoModel
 import com.edaakyil.data.exception.DataServiceException
 import java.io.File
@@ -22,51 +21,45 @@ import java.io.File
 private const val USER_INFO_EXIST_LOG_TAG = "USER_INFO_EXIST"
 
 class RegisterPasswordActivity : AppCompatActivity() {
-    private lateinit var mTextViewUsername: TextView
-    private lateinit var mEditTextPassword: EditText
-    private lateinit var mEditTextConfirmPassword: EditText
-    private lateinit var mUserRegisterInfo: UserRegisterInfoModel
+    private lateinit var mBinding: ActivityRegisterPasswordBinding
     private lateinit var mUserService: UserService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_register_password)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.registerPasswordActivityLinearLayoutMain)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         initialize()
     }
 
     private fun initialize() {
+        initBinding()
         mUserService = UserService(this)
-        mUserRegisterInfo = when {
+    }
+
+    private fun initBinding() {
+        enableEdgeToEdge()
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_register_password)
+        initModels()
+        ViewCompat.setOnApplyWindowInsetsListener(mBinding.registerPasswordActivityLinearLayoutMain) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun initModels() {
+        mBinding.activity = this
+        mBinding.userRegisterInfo = when {
             Build.VERSION.SDK_INT < 33 -> intent.getSerializableExtra(USER_INFO) as UserRegisterInfoModel
             else -> intent.getSerializableExtra(USER_INFO, UserRegisterInfoModel::class.java)!!
         }
-
-        initViews()
-    }
-
-    private fun initViews() {
-        initTextViewUsername()
-        mEditTextPassword = findViewById(R.id.registerPasswordActivityEditTextPassword)
-        mEditTextConfirmPassword = findViewById(R.id.registerPasswordActivityEditTextConfirmPassword)
-    }
-
-    private fun initTextViewUsername() {
-        mTextViewUsername = findViewById(R.id.registerPasswordActivityTextViewUsername)
-        mTextViewUsername.text = resources.getString(R.string.register_password_activity_text_view_username).format(mUserRegisterInfo.username)
+        mBinding.username = resources.getString(R.string.register_password_activity_text_view_username).format(mBinding.userRegisterInfo!!.username)
     }
 
     private fun userExists(): Boolean {
         var result = false
 
         try {
-            result = mUserService.existsByUsername(mUserRegisterInfo.username)
+            result = mUserService.existsByUsername(mBinding.userRegisterInfo!!.username)
         } catch (ex: DataServiceException) {
             Log.e(USER_INFO_EXIST_LOG_TAG, ex.message ?: "")
             Toast.makeText(this, R.string.data_problem_occurred_prompt, Toast.LENGTH_SHORT).show()
@@ -84,21 +77,23 @@ class RegisterPasswordActivity : AppCompatActivity() {
             return
         }
 
-        mUserRegisterInfo.password = password
-        mUserService.registerUserInfo(mUserRegisterInfo)
+        mBinding.userRegisterInfo?.password = password
+        mUserService.registerUserInfo(mBinding.userRegisterInfo!!)
 
         Toast.makeText(this, R.string.user_successfully_registered_prompt, Toast.LENGTH_SHORT).show()
+
+        //finish()  // oguzkaran
     }
 
-    fun onRegisterButtonClicked(view: View) {
-        val password = mEditTextPassword.text.toString()
-        val confirmPassword = mEditTextConfirmPassword.text.toString()
+    fun onRegisterButtonClicked() {
+        val password = mBinding.userRegisterInfo?.password?.trim()
+        val confirmPassword = mBinding.confirmPassword?.trim()
 
-        if (password.isBlank()) {
+        if (password!!.isBlank()) {
             AlertDialog.Builder(this)
                 .setTitle(R.string.alert_dialog_alert_title)
                 .setMessage(R.string.alert_dialog_empty_password_message)
-                .setPositiveButton(R.string.alert_dialog_ok) { _, _ -> mEditTextConfirmPassword.text.clear() }
+                .setPositiveButton(R.string.alert_dialog_ok) { _, _ -> mBinding.registerPasswordActivityEditTextPassword.text.clear(); mBinding.confirmPassword = "" }
                 .create()
                 .show()
             return
@@ -106,7 +101,7 @@ class RegisterPasswordActivity : AppCompatActivity() {
 
         if (password == confirmPassword) {
             registerUser(password)
-            File(filesDir, USERS_FORMAT.format("${mUserRegisterInfo.username}.txt")).delete()  // edaakyil
+            File(filesDir, USERS_FORMAT.format("${mBinding.userRegisterInfo?.username}.txt")).delete()  // edaakyil
             finish()
         } else
             AlertDialog.Builder(this)
@@ -116,9 +111,10 @@ class RegisterPasswordActivity : AppCompatActivity() {
                 .create()
                 .show()
 
-        mEditTextPassword.text.clear()
-        mEditTextConfirmPassword.text.clear()
+        mBinding.userRegisterInfo?.password = ""
+        mBinding.registerPasswordActivityEditTextPassword.text.clear()
+        mBinding.confirmPassword = ""
     }
 
-    fun onCloseButtonClicked(view: View) = finish()
+    fun onCloseButtonClicked() = finish()
 }
