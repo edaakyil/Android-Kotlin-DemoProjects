c5b7c331f66b3e247cdafe6f4a1aa729acb7d25b
edaakyil <edaakyil@gmail.com>
2025-02-24 12:31:21 +0300

    Bind all elements of ManagementActivity

--- 004-DemoDataBindingApp/ReadMe.md
+++ 004-DemoDataBindingApp/ReadMe.md
@@ -23,9 +23,13 @@ mBinding.userLoginInfo = mBinding.userLoginInfo!!.copy(username = "")
 <div style="background-color:#f6f6f9;padding:10px 20px;margin:2em 0;border:solid #eee 1px;">
 
 ```kt
+mBinding.username = resources.getString(R.string.username, intent.getStringExtra(USERNAME) ?: resources.getString(R.string.anonymous))
+mBinding.password = resources.getString(R.string.password, intent.getStringExtra(PASSWORD) ?: resources.getString(R.string.anonymous))
 ```
 
 ```kt
+mBinding.username = "Username: " + (intent.getStringExtra(USERNAME) ?: resources.getString(R.string.anonymous))
+mBinding.password = "Password: " + (intent.getStringExtra(PASSWORD) ?: resources.getString(R.string.anonymous))
 ```
 
 </div>
--- 004-DemoDataBindingApp/app/src/main/java/com/edaakyil/android/demodatabindingapp/ManagementActivity.kt
+++ 004-DemoDataBindingApp/app/src/main/java/com/edaakyil/android/demodatabindingapp/ManagementActivity.kt
@@ -2,57 +2,52 @@ package com.edaakyil.android.demodatabindingapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
 import com.edaakyil.android.demodatabindingapp.constant.PASSWORD
 import com.edaakyil.android.demodatabindingapp.constant.USERNAME
+import com.edaakyil.android.demodatabindingapp.databinding.ActivityManagementBinding
 
 class ManagementActivity : AppCompatActivity() {
-    private lateinit var mTextViewUsername: TextView
-    private lateinit var mTextViewPassword: TextView // edaakyil
+    private lateinit var mBinding: ActivityManagementBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_management)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.managementActivityLinearLayoutMain)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         initialize()
     }
 
     private fun initialize() {
+        initBinding()
+    }
+
+    private fun initBinding() {
+        enableEdgeToEdge()
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_management)
+        mBinding.activity = this
         initViews()
+        ViewCompat.setOnApplyWindowInsetsListener(mBinding.managementActivityLinearLayoutMain) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 
     private fun initViews() {
-        initUserInfo()
+        mBinding.username = resources.getString(R.string.username, intent.getStringExtra(USERNAME) ?: resources.getString(R.string.anonymous))
+        mBinding.password = resources.getString(R.string.password, intent.getStringExtra(PASSWORD) ?: resources.getString(R.string.anonymous))
     }
 
-    private fun initUserInfo() {
-        mTextViewUsername = findViewById(R.id.managementActivityTextViewUsername)
-        mTextViewUsername.text = resources.getString(R.string.username, intent.getStringExtra(USERNAME) ?: R.string.anonymous)
-        //mTextViewUsername.text = "Username: " + (intent.getStringExtra(USERNAME) ?: resources.getText(R.string.anonymous))
-
-        mTextViewPassword = findViewById(R.id.managementActivityTextViewPassword) // edaakyil
-        mTextViewPassword.text = resources.getString(R.string.password, intent.getStringExtra(PASSWORD) ?: R.string.anonymous) // edaakyil
-        //mTextViewPassword.text = "Password: " + (intent.getStringExtra(PASSWORD) ?: resources.getText(R.string.anonymous)) // edaakyil
-    }
-
-    fun onUsersButtonClicked(view: View) {
+    fun onUsersButtonClicked() {
         Intent(this, UsersActivity::class.java).apply { startActivity(this) }
     }
 
-    fun onUserOperationsButtonClicked(view: View) {
-
+    fun onUserOperationsButtonClicked() {
+        TODO("Not yet implement!...")
     }
 
-    fun onCloseButtonClicked(view: View) = finish()
+    fun onCloseButtonClicked() = finish()
 }
