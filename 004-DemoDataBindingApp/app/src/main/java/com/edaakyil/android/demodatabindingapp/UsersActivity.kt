3f82774702aa0bf7fb6f48136a578ceca7b31b92
edaakyil <edaakyil@gmail.com>
2025-02-25 10:54:59 +0300

    Bind views and actions of UsersActivity

--- 004-DemoDataBindingApp/app/src/main/java/com/edaakyil/android/demodatabindingapp/UsersActivity.kt
+++ 004-DemoDataBindingApp/app/src/main/java/com/edaakyil/android/demodatabindingapp/UsersActivity.kt
@@ -1,65 +1,51 @@
 package com.edaakyil.android.demodatabindingapp
 
 import android.os.Bundle
-import android.view.View
 import android.widget.ArrayAdapter
-import android.widget.EditText
-import android.widget.ListView
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.edaakyil.android.demodatabindingapp.constant.DEFAULT_USER_COUNT
 import com.edaakyil.android.demodatabindingapp.data.service.UserService
+import com.edaakyil.android.demodatabindingapp.databinding.ActivityUsersBinding
 import com.edaakyil.android.demodatabindingapp.model.UserModel
 import com.edaakyil.data.exception.DataServiceException
 
 class UsersActivity : AppCompatActivity() {
-    private lateinit var mTextViewUser: TextView
-    private lateinit var mEditTextCount: EditText
-    private lateinit var mListViewUsers: ListView
-    private lateinit var mArrayAdapterUsers: ArrayAdapter<UserModel>
+    private lateinit var mBinding: ActivityUsersBinding
     private lateinit var mUserService: UserService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_users)
 
         initialize()
     }
 
     private fun initialize() {
+        initBinding()
         mUserService = UserService(this)
-        initViews()
     }
 
-    private fun initViews() {
-        mTextViewUser = findViewById(R.id.usersActivityTextViewUser)
-        mEditTextCount = findViewById(R.id.usersActivityEditTextCount)
-        initListViewUsers()
+    private fun initBinding() {
+        enableEdgeToEdge()
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_users)
+        initModels()
     }
 
-    private fun itemClickListenerCallback(position: Int) {
-        val user = mArrayAdapterUsers.getItem(position)
-
-        Toast.makeText(this, user?.name, Toast.LENGTH_SHORT).show()
+    private fun initModels() {
+        mBinding.activity = this
+        mBinding.countStr = ""
+        mBinding.nameSelectedUser = ""
+        mBinding.listViewAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, ArrayList<UserModel>())
     }
 
-    private fun initListViewUsers() {
-        mListViewUsers = findViewById(R.id.usersActivityListViewUsers)
-        mListViewUsers.setOnItemClickListener { _, _, position, _ -> itemClickListenerCallback(position) }
-    }
-
-    fun onLoadUsersButtonClicked(view: View) {
+    fun onLoadUsersButtonClicked() {
         try {
-            if (this::mArrayAdapterUsers.isInitialized)
-                mArrayAdapterUsers.clear()
+            mBinding.listViewAdapter!!.clear()
 
-            val countStr = mEditTextCount.text.toString().trim()
-            mEditTextCount.text.clear()
-
+            val countStr = mBinding.countStr!!.trim()
             var count = DEFAULT_USER_COUNT  // max value of count (default value)
 
             if (countStr.isNotBlank())
@@ -67,13 +53,13 @@ class UsersActivity : AppCompatActivity() {
 
             if (count <= 0) {
                 Toast.makeText(this, R.string.value_must_be_positive_prompt, Toast.LENGTH_SHORT).show()
+                mBinding.countStr = ""
                 return
             }
 
             val users = mUserService.findUsers(count)  // Must be asynchronous
-
-            mArrayAdapterUsers = ArrayAdapter(this, android.R.layout.simple_list_item_1, users)
-                .apply { mListViewUsers.adapter = this }
+            mBinding.listViewAdapter!!.addAll(users)
+            mBinding.countStr = ""
         } catch (_: NumberFormatException) {
             AlertDialog.Builder(this)
                 .setTitle(R.string.alert_dialog_alert_title)
@@ -86,5 +72,13 @@ class UsersActivity : AppCompatActivity() {
         } catch (ex: Exception) {
             Toast.makeText(this, R.string.problem_occurred_prompt, Toast.LENGTH_SHORT).show()
         }
+    }
+
+    fun onListViewItemClick(position: Int) {
+        val user = mBinding.listViewAdapter!!.getItem(position)
+
+        mBinding.nameSelectedUser = user?.name
+
+        Toast.makeText(this, user?.name, Toast.LENGTH_SHORT).show()
     }
 }
