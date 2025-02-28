e93096cc5d7e82d05f039db193ef3cc2754a10a8
edaakyil <edaakyil@gmail.com>
2025-02-28 12:49:58 +0300

    Second implementation of Hilt - Context injected by default

--- 006-DemoPaymentApp/app/src/main/java/com/edaakyil/android/demopaymentapp/application/module/datetime/DateTimeFormatterModule.kt
+++ 006-DemoPaymentApp/app/src/main/java/com/edaakyil/android/demopaymentapp/application/module/datetime/DateTimeFormatterModule.kt
@@ -1,8 +1,11 @@
 package com.edaakyil.android.demopaymentapp.application.module.datetime
 
+import android.content.Context
+import android.widget.Toast
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import java.time.format.DateTimeFormatter
 import javax.inject.Singleton
@@ -12,5 +15,9 @@ import javax.inject.Singleton
 object DateTimeFormatterModule {
     @Provides
     @Singleton
-    fun provideDateTimeFormatter(): DateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")
+    fun provideDateTimeFormatter(@ApplicationContext context: Context): DateTimeFormatter {
+        Toast.makeText(context, "Created provideDateTimeFormatter", Toast.LENGTH_SHORT).show()
+
+        return DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")
+    }
 }
--- 006-DemoPaymentApp/app/src/main/java/com/edaakyil/android/demopaymentapp/application/module/datetime/LocalDateTimeModule.kt
+++ 006-DemoPaymentApp/app/src/main/java/com/edaakyil/android/demopaymentapp/application/module/datetime/LocalDateTimeModule.kt
@@ -1,14 +1,21 @@
 package com.edaakyil.android.demopaymentapp.application.module.datetime
 
+import android.content.Context
+import android.widget.Toast
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
 import java.time.LocalDateTime
 
 @Module
 @InstallIn(ActivityComponent::class)
 object LocalDateTimeModule {
     @Provides
-    fun provideLocalDateTime(): LocalDateTime = LocalDateTime.now()
+    fun provideLocalDateTime(@ApplicationContext context: Context): LocalDateTime {
+        Toast.makeText(context, "Created provideLocalDateTime", Toast.LENGTH_SHORT).show()
+
+        return LocalDateTime.now()
+    }
 }
