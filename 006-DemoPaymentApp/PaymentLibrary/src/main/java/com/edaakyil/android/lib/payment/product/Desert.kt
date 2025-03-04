24c5e68c8c2a35a3fddbf2a97ca1637115c2fd70
edaakyil <edaakyil@gmail.com>
2025-03-04 17:58:55 +0300

    Update

--- 006-DemoPaymentApp/PaymentLibrary/src/main/java/com/edaakyil/android/lib/payment/product/Desert.kt
+++ 006-DemoPaymentApp/PaymentLibrary/src/main/java/com/edaakyil/android/lib/payment/product/Desert.kt
@@ -3,9 +3,11 @@ package com.edaakyil.android.lib.payment.product
 import android.content.Context
 import android.widget.Toast
 import dagger.hilt.android.qualifiers.ActivityContext
+import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
 import kotlin.random.Random
 
+@ActivityScoped
 class Desert @Inject constructor(@ActivityContext var context: Context) : BaseProduct("Desert", Random.nextDouble(1.0, 100.0)) {
     override fun calculatePayment(amount: Double): Double {
         Toast.makeText(context, "Name: $name:\nAmount -> $amount,\nUnit Price -> $unitPrice", Toast.LENGTH_SHORT).show()
--- 006-DemoPaymentApp/PaymentLibrary/src/main/java/com/edaakyil/android/lib/payment/product/Menu.kt
+++ 006-DemoPaymentApp/PaymentLibrary/src/main/java/com/edaakyil/android/lib/payment/product/Menu.kt
@@ -1,10 +1,16 @@
 package com.edaakyil.android.lib.payment.product
 
+import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
 import javax.inject.Named
 import kotlin.random.Random
 
-class Menu @Inject constructor(@Named("food") var product1: BaseProduct, @Named("drink") var product2: BaseProduct, @Named("desert") var product3: BaseProduct) : BaseProduct("Menu", Random.nextDouble(1.0, 100.0)), IProductPayment {
+@ActivityScoped
+class Menu @Inject constructor(
+    @Named("food") var product1: BaseProduct,
+    @Named("drink") var product2: BaseProduct,
+    @Named("desert") var product3: BaseProduct
+) : BaseProduct("Menu", Random.nextDouble(1.0, 100.0)){
     override fun calculatePayment(amount: Double): Double {
         return amount * (product1.calculatePayment(1.0) + product2.calculatePayment(1.0) + product3.calculatePayment(1.0))
     }
