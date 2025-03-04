903250e14d94dbea7f98e7a114d86e4c28772755
edaakyil <edaakyil@gmail.com>
2025-03-04 16:27:43 +0300

    Update

--- 006-DemoPaymentApp/PaymentLibrary/src/main/java/com/edaakyil/android/lib/payment/product/MenuPayment.kt
+++ 006-DemoPaymentApp/PaymentLibrary/src/main/java/com/edaakyil/android/lib/payment/product/MenuPayment.kt
@@ -3,6 +3,8 @@ package com.edaakyil.android.lib.payment.product
 import javax.inject.Inject
 import kotlin.random.Random
 
-class MenuPayment @Inject constructor() : BaseProduct("", Random.nextDouble(1.0, 100.0)), IProductPayment {
-    override fun calculatePayment(amount: Double) = amount * unitPrice * 1.6
+class MenuPayment @Inject constructor(private var foodPayment: FoodPayment, private var drinkPayment: DrinkPayment) : BaseProduct("Menu", Random.nextDouble(1.0, 100.0)), IProductPayment {
+    override fun calculatePayment(amount: Double): Double {
+        return amount * (foodPayment.calculatePayment(1.0) + drinkPayment.calculatePayment(1.0))
+    }
 }
