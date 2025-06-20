f41569eb73d03dbb063ff3fc3e3e5e2a81c4751c
edaakyil <edaakyil@gmail.com>
2025-06-20 15:42:50 +0300

    Initial commit

---
+++ 012-ImageProcessingServer/Server/src/main/java/com/edaakyil/java/app/ApplicationJavaGradleKotlinApplication.java
@@ -0,0 +1,13 @@
+package com.edaakyil.java.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationJavaGradleKotlinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationJavaGradleKotlinApplication.class, args);
+	}
+
+}
