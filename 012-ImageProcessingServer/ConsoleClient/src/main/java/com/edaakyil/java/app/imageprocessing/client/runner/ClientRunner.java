d792dbe1e672ad1541cbdfe273432455f18f9354
edaakyil <edaakyil@gmail.com>
2025-06-20 19:04:17 +0300

    Update

--- 012-ImageProcessingServer/ConsoleClient/src/main/java/com/edaakyil/java/app/imageprocessing/client/runner/ClientRunner.java
+++ 012-ImageProcessingServer/ConsoleClient/src/main/java/com/edaakyil/java/app/imageprocessing/client/runner/ClientRunner.java
@@ -10,17 +10,15 @@ import java.util.concurrent.ExecutorService;
 @Configuration
 public class ClientRunner implements ApplicationRunner {
     private final Client m_client;
-    private final ExecutorService m_executorService;
 
-    public ClientRunner(Client client, ExecutorService executorService)
+    public ClientRunner(Client client)
     {
         m_client = client;
-        m_executorService = executorService;
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception
     {
-        m_executorService.execute(m_client::run);
+        new Thread(m_client::run).start();
     }
 }
