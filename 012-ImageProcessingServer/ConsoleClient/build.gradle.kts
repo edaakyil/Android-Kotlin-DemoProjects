dece0f841f3604228736fb5df0c9964d5839fe15
edaakyil <edaakyil@gmail.com>
2025-06-20 15:43:28 +0300

    Initial commit

---
+++ 012-ImageProcessingServer/ConsoleClient/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.3"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.edaakyil"
+version = "1.0.0"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenLocal()
+	mavenCentral()
+	maven {
+		url = uri("https://raw.github.com/oguzkaran/javaapp2-jan-2024-maven-repo/main")
+		url = uri("https://raw.github.com/oguzkaran/javaapp2-jan-2024-karandev-maven-repo/main")
+	}
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+
+	compileOnly("org.projectlombok:lombok:1.18.38")
+	annotationProcessor("org.projectlombok:lombok")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
