6aa7a41f6d0aa73d5bd88a60363ee3e3f0c921fa
edaakyil <edaakyil@gmail.com>
2025-06-20 18:11:50 +0300

    Update

--- 012-ImageProcessingServer/README.md
+++ 012-ImageProcessingServer/README.md
@@ -44,3 +44,108 @@ m_executorService.execute(m_server::start);
 
 </div>
 
+
+<div style="background-color:#f6f6f9;padding:10px 20px;margin:2em 0;border:solid #eee 1px;">
+
+```java
+```
+
+```java
+```
+
+</div>
+
+<div style="background-color:#f6f6f9;padding:10px 20px;margin:2em 0;border:solid #eee 1px;">
+
+```java
+```
+
+```java
+```
+
+</div>
+
+
+<div style="background-color:#f6f6f9;padding:10px 20px;margin:2em 0;border:solid #eee 1px;">
+
+```java
+```
+
+```java
+```
+
+</div>
+
+<div style="background-color:#f6f6f9;padding:10px 20px;margin:2em 0;border:solid #eee 1px;">
+
+```java
+```
+
+```java
+```
+
+</div>
+
+
+<div style="background-color:#f6f6f9;padding:10px 20px;margin:2em 0;border:solid #eee 1px;">
+
+```java
+```
+
+```java
+```
+
+</div>
+
+<div style="background-color:#f6f6f9;padding:10px 20px;margin:2em 0;border:solid #eee 1px;">
+
+```java
+```
+
+```java
+```
+
+</div>
+
+
+<div style="background-color:#f6f6f9;padding:10px 20px;margin:2em 0;border:solid #eee 1px;">
+
+```java
+```
+
+```java
+```
+
+</div>
+
+<div style="background-color:#f6f6f9;padding:10px 20px;margin:2em 0;border:solid #eee 1px;">
+
+```java
+```
+
+```java
+```
+
+</div>
+
+
+<div style="background-color:#f6f6f9;padding:10px 20px;margin:2em 0;border:solid #eee 1px;">
+
+```java
+```
+
+```java
+```
+
+</div>
+
+<div style="background-color:#f6f6f9;padding:10px 20px;margin:2em 0;border:solid #eee 1px;">
+
+```java
+```
+
+```java
+```
+
+</div>
+
--- 012-ImageProcessingServer/Server/src/main/java/com/edaakyil/java/app/imageprocessing/server/Server.java
+++ 012-ImageProcessingServer/Server/src/main/java/com/edaakyil/java/app/imageprocessing/server/Server.java
@@ -1,14 +1,36 @@
 package com.edaakyil.java.app.imageprocessing.server;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 
 @Component
 @Slf4j
 public class Server {
     private final ExecutorService m_executorService;
+
+    @Value("${app.server.port}")
+    private int m_port;
+
+    /**
+     * Client ile ilgili işlemler bu akış içerisinde karşılanacak
+     * @param socket
+     */
+    private void handleClient(Socket socket)
+    {
+        try (socket) {
+            log.info("Client connected from {}:{}", socket.getInetAddress(), socket.getPort());
+        } catch (IOException ex) {
+            log.error("IO Problem occurred while client connected: {}", ex.getMessage());
+        } catch (Exception ex) {
+            log.error("Problem occurred while client connected: {}", ex.getMessage());
+        }
+    }
 
     public Server(ExecutorService executorService)
     {
@@ -20,6 +42,18 @@ public class Server {
      */
     public void start()
     {
-        log.info("Server starting...");
+        log.info("Starting server on port: {}", m_port);
+
+        try (var serverSocket = new ServerSocket(m_port)) {
+            while (true) {
+                var socket = serverSocket.accept();
+
+                m_executorService.execute(() -> handleClient(socket));
+            }
+        } catch (IOException ex) {
+            log.error("IO Problem occurred while server is waiting: {}", ex.getMessage());
+        } catch (Exception ex) {
+            log.error("Problem occurred while server is waiting: {}", ex.getMessage());
+        }
     }
 }
